62f472ea0abe2f7264a17cda17f2e4037d43732c
montlikadani <montlikada@gmail.com>
2022-03-07 20:59:07 +0100

    Provide browser history for router instead

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import TLRoot from "./index.jsx";
 
+const history = createBrowserHistory();
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter history={history}>
       <Routes>
         <Route exact path="/" element={<TLRoot />} />
       </Routes>
